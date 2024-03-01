1658c40a7d849514cafeb3a339fa1fab015b7b0d
Chris Jobin <c.r.jobin@gmail.com>
2024-02-29 22:41:36 -0600

    various set up

---
+++ BlazorDynamicForm/BlazorDynamicForm/Attributes/Panel/DynamicPanelAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDynamicForm.Attributes.Panel
+{
+    public class DynamicPanelAttribute : DynamicDisplayAttribute
+    {
+        public DynamicPanelAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
