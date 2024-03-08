930fc1b96516f388d26ff568663a966735128312
Chris Jobin <c.r.jobin@gmail.com>
2024-03-08 15:41:54 -0600

    Moved display templates into folder

--- BlazorDynamicForm/BlazorDynamicComponents/Display/DynamicDisplayTemplate.razor.cs
+++ BlazorDynamicForm/BlazorDynamicComponents/Display/DynamicDisplayTemplate.razor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorDynamicComponents
+namespace BlazorDynamicComponents.Display
 {
     public partial class DynamicDisplayTemplate
     {
